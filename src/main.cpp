aa71400ab2bccae5c419950ced3eea56448a867b
Pokito <trirayan@gmail.com>
2025-04-08 14:59:51 -0500

    Spectro Updated

--- src/main.cpp
+++ src/main.cpp
@@ -9,18 +9,25 @@
 
 #define UBLOX_CUSTOM_MAX_WAIT (250u)
 
+#define ADC_BITS (8)
+#define ADC_DIVIDER static_cast<float>((1 << ADC_BITS) - 1)
+
 #define SPEC_TRG PA_0
 #define SPEC_ST PA_1
 #define SPEC_CLK PB_12
-#define SPEC_VIDEO PB_0   
+#define SPEC_VIDEO PA_4
 #define WHITE_LED PA_5
 #define LASER_404 PA_6
 
 #define SPEC_CHANNELS 288      // จำนวนช่องข้อมูลจากเซ็นเซอร์
-#define WATER_THRESHOLD 250    // ระดับ threshold สำหรับการตรวจจับน้ำ
+#define WATER_THRESHOLD 0.24f    // ระดับ threshold สำหรับการตรวจจับน้ำ
 #define WATER_CHANNEL_START 50 // ช่วง channel ที่เกี่ยวข้องกับน้ำ
 #define WATER_CHANNEL_END 200
 
+#define DELAY(MS) vTaskDelay(pdMS_TO_TICKS(MS))
+
+const int groupSize = 18;
+const int numGroups = SPEC_CHANNELS / groupSize; // =16
 constexpr int AVERAGE_SAMPLES = 10;
 
 // Devices
@@ -31,10 +38,10 @@ SFE_UBLOX_GNSS gps;
 
 // Variables
 
-float prevSpeed = 0.0;  
+float prevSpeed = 0.0;
 unsigned int prevTime = 0;
 uint8_t sat = 0;
-uint16_t spec[SPEC_CHANNELS];
+float spec[SPEC_CHANNELS];
 
 // Data
 
@@ -65,36 +72,36 @@ void taskReadGpsLidar(void *)
 {
     for (;;)
     {
-        //gps
+        // gps
         data.gps_lat = static_cast<double>(gps.getLatitude(UBLOX_CUSTOM_MAX_WAIT)) * 1.e-7;
         data.gps_lon = static_cast<double>(gps.getLongitude(UBLOX_CUSTOM_MAX_WAIT)) * 1.e-7;
         data.gps_alt = static_cast<float>(gps.getAltitude(UBLOX_CUSTOM_MAX_WAIT)) * 1.e-3f; // Ellipsoid
         data.timeStamp = gps.getUnixEpoch(UBLOX_CUSTOM_MAX_WAIT);
         data.velocity = gps.getGroundSpeed(UBLOX_CUSTOM_MAX_WAIT) / 1000.0;
         sat = gps.getSIV(UBLOX_CUSTOM_MAX_WAIT);
-        
-        //Acceleration Calculation
-        unsigned long currentTime = millis();  
 
-        if (prevTime != 0)  
+        // Acceleration Calculation
+        unsigned long currentTime = millis();
+
+        if (prevTime != 0)
         {
-          float deltaTime = (currentTime - prevTime) / 1000.0; // Convert ms to seconds
-    
-          if (deltaTime > 0)
-          {
-            data.acceleration = (data.velocity - prevSpeed) / deltaTime;
-          }
+            float deltaTime = (currentTime - prevTime) / 1000.0; // Convert ms to seconds
+
+            if (deltaTime > 0)
+            {
+                data.acceleration = (data.velocity - prevSpeed) / deltaTime;
+            }
         }
         prevSpeed = data.velocity;
         prevTime = currentTime;
-        
-        //lidar
+
+        // lidar
         float total = 0;
         for (size_t i = 0; i < AVERAGE_SAMPLES; i++)
         {
-            float distance = static_cast<float>(lidar.distance());  //cm
+            float distance = static_cast<float>(lidar.distance()); // cm
             total += distance;
-            vTaskDelay(pdMS_TO_TICKS(10));
+            DELAY(10);
         }
         data.d_lidar = total / static_cast<float>(AVERAGE_SAMPLES);
 
@@ -106,30 +113,48 @@ void taskReadSpec(void *)
 {
     for (;;)
     {
+        taskENTER_CRITICAL();
         readSpectrometer();
-        if (detectWater()) {
-            data.water_detect = true;
-        } else {
-            data.water_detect = false;
-        }
-        vTaskDelay(pdMS_TO_TICKS(500));
+        data.water_detect = detectWater();
+        taskEXIT_CRITICAL();
+
+        vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
 
-void checkVoltage(void * ) 
-{
-    for(;;)
-    {
-        data.voltage = analogRead(SPEC_VIDEO) * (5.0 / 1023.0); // คำนวณแรงดัน
-        vTaskDelay(pdMS_TO_TICKS(500));
-    }
-}
-
-void taskPrint(void *) 
+void averageData(void *)
 {
     for (;;)
     {
-        printData(); 
+        for (int i = 0; i < numGroups; i++)
+        {
+            float sum = 0;
+
+            for (int j = 0; j < groupSize; j++)
+            {
+                sum += spec[i * groupSize + j];
+            }
+
+            data.avgSpec[i] = (sum / groupSize) * 255;
+        }
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
+
+void checkVoltage(void *)
+{
+    for (;;)
+    {
+        data.voltage = analogRead(digitalPinToAnalogInput(pinNametoDigitalPin(SPEC_VIDEO))) * (5.0 / 1023.0); // คำนวณแรงดัน
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
+}
+
+void taskPrint(void *)
+{
+    for (;;)
+    {
+        printData();
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
@@ -138,7 +163,7 @@ void taskSend(void *)
 {
     for (;;)
     {
-        //rfd900x.write(reinterpret_cast<uint8_t *>(&data), sizeof(Data));
+        // rfd900x.write(reinterpret_cast<uint8_t *>(&data), sizeof(Data));
         rfd900x.print("H");
         rfd900x.print(data.counter);
         rfd900x.print(", ");
@@ -164,7 +189,8 @@ void taskSend(void *)
         for (int i = 0; i < 16; i++)
         {
             rfd900x.print(data.avgSpec[i]);
-            if (i<15) {
+            if (i < 15)
+            {
                 rfd900x.print(", ");
             }
         }
@@ -183,16 +209,18 @@ void setup()
 
     Wire.begin();
 
-    pinMode(SPEC_CLK, OUTPUT);
-    pinMode(SPEC_ST, OUTPUT);
-    pinMode(LASER_404, OUTPUT);
-    pinMode(WHITE_LED, OUTPUT);
+    pinMode(pinNametoDigitalPin(SPEC_CLK), OUTPUT);
+    pinMode(pinNametoDigitalPin(SPEC_ST), OUTPUT);
+    pinMode(pinNametoDigitalPin(LASER_404), OUTPUT);
+    pinMode(pinNametoDigitalPin(WHITE_LED), OUTPUT);
+    pinMode(pinNametoDigitalPin(SPEC_VIDEO), INPUT_ANALOG);
+    analogReadResolution(ADC_BITS);
 
-    digitalWrite(SPEC_CLK, HIGH); // ตั้งค่าเริ่มต้นให้ SPEC_CLK สูง
-    digitalWrite(SPEC_ST, LOW);   // ตั้งค่าเริ่มต้นให้ SPEC_ST ต่ำ
+    digitalWriteFast(SPEC_CLK, HIGH); // ตั้งค่าเริ่มต้นให้ SPEC_CLK สูง
+    digitalWriteFast(SPEC_ST, LOW);   // ตั้งค่าเริ่มต้นให้ SPEC_ST ต่ำ
 
-    lidar.begin(0, true); 
-    lidar.configure(0);  
+    lidar.begin(0, true);
+    lidar.configure(0);
 
     if (gps.begin(0x42, UBLOX_CUSTOM_MAX_WAIT))
     {
@@ -202,8 +230,9 @@ void setup()
         gps.setDynamicModel(DYN_MODEL_AUTOMOTIVE, VAL_LAYER_RAM_BBR, UBLOX_CUSTOM_MAX_WAIT);
     }
 
-    xTaskCreate(taskReadGpsLidar, "", 2048, nullptr, 2, nullptr);
-    xTaskCreate(taskReadSpec, "", 2048, nullptr, 2, nullptr);
+    //xTaskCreate(taskReadGpsLidar, "", 2048, nullptr, 2, nullptr);
+    xTaskCreate(taskReadSpec, "", 4096, nullptr, 2, nullptr);
+    xTaskCreate(averageData, "", 1024, nullptr, 2, nullptr);
     xTaskCreate(checkVoltage, "", 1024, nullptr, 2, nullptr);
     xTaskCreate(taskPrint, "", 1024, nullptr, 2, nullptr);
     xTaskCreate(taskSend, "", 1024, nullptr, 2, nullptr);
@@ -213,82 +242,84 @@ void setup()
 void readSpectrometer()
 {
     static constexpr int delayTime = 1; // หน่วงเวลาระหว่างการอ่าน
-    digitalWrite(WHITE_LED, HIGH);
-    digitalWrite(SPEC_CLK, LOW);
+
+    digitalWriteFast(WHITE_LED, HIGH);
+    digitalWriteFast(SPEC_CLK, LOW);
     delayMicroseconds(delayTime);
-    digitalWrite(SPEC_CLK, HIGH);
+    digitalWriteFast(SPEC_CLK, HIGH);
     delayMicroseconds(delayTime);
-    digitalWrite(SPEC_CLK, LOW);
-    digitalWrite(SPEC_ST, HIGH); // ส่ง Start pulse
+    digitalWriteFast(SPEC_CLK, LOW);
+    digitalWriteFast(SPEC_ST, HIGH); // ส่ง Start pulse
     delayMicroseconds(delayTime);
 
     // ทำ clock cycle 15 ครั้ง
     for (int i = 0; i < 15; i++)
     {
-        digitalWrite(SPEC_CLK, HIGH);
+        digitalWriteFast(SPEC_CLK, HIGH);
         delayMicroseconds(delayTime);
-        digitalWrite(SPEC_CLK, LOW);
+        digitalWriteFast(SPEC_CLK, LOW);
         delayMicroseconds(delayTime);
     }
 
-    digitalWrite(SPEC_ST, LOW); // ปิด Start pulse
+    digitalWriteFast(SPEC_ST, LOW); // ปิด Start pulse
 
     // ทำ clock cycle 85 ครั้ง
     for (int i = 0; i < 85; i++)
     {
-        digitalWrite(SPEC_CLK, HIGH);
+        digitalWriteFast(SPEC_CLK, HIGH);
         delayMicroseconds(delayTime);
-        digitalWrite(SPEC_CLK, LOW);
+        digitalWriteFast(SPEC_CLK, LOW);
         delayMicroseconds(delayTime);
     }
 
-    digitalWrite(SPEC_CLK, HIGH);
+    digitalWriteFast(SPEC_CLK, HIGH);
     delayMicroseconds(delayTime);
-    digitalWrite(SPEC_CLK, LOW);
+    digitalWriteFast(SPEC_CLK, LOW);
     delayMicroseconds(delayTime);
 
     // อ่านค่าจาก SPEC_VIDEO
     for (int i = 0; i < SPEC_CHANNELS; i++)
     {
-        uint32_t sum = 0; // สำหรับค่าเฉลี่ย
+        float sum = 0; // สำหรับค่าเฉลี่ย
         for (int j = 0; j < AVERAGE_SAMPLES; j++)
-        {
-            sum += analogRead(SPEC_VIDEO); // อ่านค่า ADC หลายครั้ง
-        }
-        spec[i] = sum / AVERAGE_SAMPLES; // คำนวณค่าเฉลี่ย
-        digitalWrite(SPEC_CLK, HIGH);
+            delayMicroseconds(delayTime);
+            sum += analogRead(digitalPinToAnalogInput(pinNametoDigitalPin(SPEC_VIDEO))); // อ่านค่า ADC หลายครั้ง
+        spec[i] = sum / static_cast<float>(AVERAGE_SAMPLES) / ADC_DIVIDER; // คำนวณค่าเฉลี่ย
+        digitalWriteFast(SPEC_CLK, HIGH);
         delayMicroseconds(delayTime);
-        digitalWrite(SPEC_CLK, LOW);
+        digitalWriteFast(SPEC_CLK, LOW);
         delayMicroseconds(delayTime);
     }
 
-    digitalWrite(SPEC_ST, HIGH);
+    digitalWriteFast(SPEC_ST, HIGH);
 
     // Clock cycle อีก 7 ครั้ง
     for (int i = 0; i < 7; i++)
     {
-        digitalWrite(SPEC_CLK, HIGH);
+        digitalWriteFast(SPEC_CLK, HIGH);
         delayMicroseconds(delayTime);
-        digitalWrite(SPEC_CLK, LOW);
+        digitalWriteFast(SPEC_CLK, LOW);
         delayMicroseconds(delayTime);
     }
 
-    digitalWrite(SPEC_CLK, HIGH);
+    digitalWriteFast(SPEC_CLK, HIGH);
     delayMicroseconds(delayTime);
 }
 
 bool detectWater()
 {
     int waterSignal = 0;
+    float avg = 0.f;
 
     // ตรวจสอบค่าช่องข้อมูลที่เกี่ยวข้อง
-    for (int i = WATER_CHANNEL_START; i <= WATER_CHANNEL_END; i++)
+    for (int i = WATER_CHANNEL_START; i < WATER_CHANNEL_END; i++)
     {
-        if (spec[i] > WATER_THRESHOLD)
-        {
-            waterSignal++;
-        }
+        waterSignal += spec[i] > WATER_THRESHOLD;
+        avg += spec[i];
     }
+
+    Serial.print("WATER AVG = ");
+    Serial.println(avg / (WATER_CHANNEL_END - WATER_CHANNEL_START));
 
     // หากค่าช่องที่เกิน Threshold มากกว่าครึ่งของช่วง ให้ถือว่าตรวจจับน้ำได้
     return waterSignal > (WATER_CHANNEL_END - WATER_CHANNEL_START) / 2;
@@ -297,30 +328,55 @@ bool detectWater()
 void printData()
 {
     Serial.println("Sensor Data:");
-    Serial.print("Counter: "); Serial.println(data.counter);
-    Serial.print("Timestamp: "); Serial.println(data.timeStamp);
-    Serial.print("Satellite Available: "); Serial.println(sat);
-    Serial.print("GPS Latitude: "); Serial.println(data.gps_lat, 6);
-    Serial.print("GPS Longitude: "); Serial.println(data.gps_lon, 6);
-    Serial.print("GPS Altitude: "); Serial.println(data.gps_alt);
-    Serial.print("LIDAR Distance: "); Serial.print(data.d_lidar); Serial.println(" cm");
-    Serial.print("Velocity: "); Serial.println(data.velocity);
-    Serial.print("Acceleration: "); Serial.println(data.acceleration);
-    Serial.print("Voltage: "); Serial.println(data.voltage);
-    Serial.print("Water Detected: "); Serial.println(data.water_detect ? "Yes" : "No");
-    
+    Serial.print("Counter: ");
+    Serial.println(data.counter);
+    Serial.print("Timestamp: ");
+    Serial.println(data.timeStamp);
+    Serial.print("Satellite Available: ");
+    Serial.println(sat);
+    Serial.print("GPS Latitude: ");
+    Serial.println(data.gps_lat, 6);
+    Serial.print("GPS Longitude: ");
+    Serial.println(data.gps_lon, 6);
+    Serial.print("GPS Altitude: ");
+    Serial.println(data.gps_alt);
+    Serial.print("LIDAR Distance: ");
+    Serial.print(data.d_lidar);
+    Serial.println(" cm");
+    Serial.print("Velocity: ");
+    Serial.println(data.velocity);
+    Serial.print("Acceleration: ");
+    Serial.println(data.acceleration);
+    Serial.print("Voltage: ");
+    Serial.println(data.voltage);
+    Serial.print("Water Detected: ");
+    Serial.println(data.water_detect ? "Yes" : "No");
+
+    Serial.print("Full Spectral: ");
+    for (int i = 0; i < SPEC_CHANNELS; i++)
+    {
+        Serial.print(spec[i]);
+        if (i < SPEC_CHANNELS - 1)
+        {
+            Serial.print(',');
+        }
+    }
+    Serial.println();
+
     Serial.print("Spectral Averages: ");
-    for (int i = 0; i < 16; i++)
+    for (int i = 0; i < numGroups; i++)
     {
         Serial.print(data.avgSpec[i]);
-        if (i<15) {
+        if (i < numGroups - 1)
+        {
             Serial.print(", ");
         }
     }
     Serial.println();
-    
+
     Serial.println("----------------------");
 }
 
 void loop()
-{}
+{
+}
